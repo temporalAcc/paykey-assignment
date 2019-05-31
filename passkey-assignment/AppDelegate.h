e15d3d13fc625f881c2723946ce9169ade6d57f5
Igor Pivnik <consul87@gmail.com>
2019-05-31 13:39:39 +0300

    project created

---
+++ passkey-assignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  passkey-assignment
+//
+//  Created by Igor Pivnyk on 31/05/2019.
+//  Copyright © 2019 Igor Pivnyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ passkey-assignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  passkey-assignment
+//
+//  Created by Igor Pivnyk on 31/05/2019.
+//  Copyright © 2019 Igor Pivnyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
